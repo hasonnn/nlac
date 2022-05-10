30d188018186eecfd47bb5a53dcbcfa5960f9138
Jason Chan <jchan321738@gmail.com>
2022-05-09 23:19:29 -0700

    Update Church Online

--- src/Components/Online/Online.js
+++ src/Components/Online/Online.js
@@ -12,23 +12,8 @@ const Online = () => {
                     <h1 className="online-heading">Church Online</h1> 
                     <p className="online-subheading">Join our Sunday service online via Facebook live stream at 10:30AM.
                     <br/>You can find the live stream and follow us on Facebook for updates!
-                    <br/>Click here for Facebook - <u><a href="https://www.facebook.com/newlifealliancechurchbc" target="_blank" rel="noreferrer">@newlifealliancechurchbc</a></u></p>
-                </div>
-                <div className="online-content">
-                    <h1 className="online-heading">In-Person Service</h1> 
-                    <p className="online-subheading">Join our Sunday service in person!</p>
-                </div>
-                <div className="online-covid">
-                    <p className="online-subheading"><u><b>Covid-19 Safety Rules:</b></u></p>
-                    <div className="online-list">
-                        <ol>
-                            <li>Sanitize your hands when entering the Rec Center.</li>
-                            <li>Please initial next to your name and correct date.</li>
-                            <li>If it is your first time with us, please write down your contact information and sign accordingly.</li>
-                            <li>Masks are recommended for everyone 12 years of age and older who are not fully vaccinated.</li>
-                            <li>Please follow safety protocols and respect people's personal space.</li>
-                        </ol>
-                    </div>
+                    <br/>Click here for Facebook - <u><a href="https://www.facebook.com/newlifealliancechurchbc" target="_blank" rel="noreferrer">@newlifealliancechurchbc</a></u>
+                    </p>
                 </div>
             </div>
         </>
