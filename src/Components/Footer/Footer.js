ff3c4b1d4b487d5d56e073d95ee419dac8b04043
Jason Chan <jchan321738@gmail.com>
2022-10-29 22:37:17 -0700

    Location on Schedule & Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,14 +8,6 @@ const Footer = () => {
                 <div className="footer-content">
                     <div className="footer-nlac"><img id="footer-alliance-logo" src="../../.././alliance-logo.svg" alt="alliance logo" />New Life Alliance Church</div>
                 </div>
-                <div className="footer-content">
-                    <div>
-                    <p className="footer-info">Memorial Community Recreation Centre<br/>123 East 23rd Street, North Vancouver, BC V7N 2L3</p>
-                    <p className="footer-info">Sunday Service: 10:30am</p>
-                    <br/>
-                    <p className="footer-report"><a href="https://www.cmacan.org/report-sexual-misconduct/">Reporting Sexual Misconduct</a></p>
-                    </div>
-                </div>
             </div>
         </>
     )
